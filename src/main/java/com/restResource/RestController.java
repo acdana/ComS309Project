1644437d37a2d895f66def0a3122a00ecf18bd96
Alex Dana <acdana@iastate.edu>
2014-09-24 22:05:13 -0500

    I've altered our RestController so that it has the ability to create new users via 309/T11/createNewUser/{username}/{password}/{email}. I think that I will end up moving this to a separate method.

--- src/main/java/com/restResource/RestController.java
+++ src/main/java/com/restResource/RestController.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.entities.User;
 import com.persistenceResource.DataController;
 import com.persistenceResource.FactoryStartup;
 
@@ -30,19 +32,31 @@ public class RestController {
     		return "Error message of some kind";
     	}
       }
-      
      
       
-    //  @GET
-    //  @Produces(MediaType.TEXT_PLAIN)
-    //  @Path("/getAllUsernames")
-   //   public String createNewUser() {
-   // 	  
-   // 	//ArrayList<String> userNames = dataController.getAllUsernames(em);
-   // 	em.close();
-   //    // return userNames.toString();
-  //   return "Test";
-   //   }
+      @GET
+      @Produces(MediaType.TEXT_PLAIN)
+      @Path("/createNewUser/{username}/{password}/{email}")
+      public String createNewUser(@PathParam("username") String username, @PathParam("password") String password, @PathParam("email") String email) {
+    	 
+    	  try {
+    	  em.getTransaction().begin();
+    	  User user = new User();
+    	  user.setUsername(username);
+    	  user.setPassword(password);
+    	  user.setEmail(email);
+    	  user.setAccountStatus("Clear");
+    	  user.setUserType("Basic");
+    	  user.setPenalties(0);
+    	  em.persist(user);
+    	  em.getTransaction().commit();
+    	  em.close();
+    	  return "Success";
+    	  }
+    	  catch(Exception e) {
+    		  return "Failure";
+    	  }
+      }
 
 
 }
