882a73363fac0924062d9adbfb1b3dbc6ec3a5b7
drcain <drcain@iastate.edu>
2014-11-17 23:36:58 -0600

    Fixed submit bio function

--- src/main/webapp/frontEnd/editProfile/editProfile.js
+++ src/main/webapp/frontEnd/editProfile/editProfile.js
@@ -5,6 +5,11 @@ function logout() {
     
 }
 
+function getUsername() {
+	var username = getCredentials().substr(0, getCredentials().indexOf(':'));
+	return username.substr(username.indexOf(' ')+1);
+}
+
 function deleteCookie() {
 	  document.cookie = "ourCookie=Authorization=; path=/";
 	}
@@ -14,7 +19,7 @@ $('#subBio').click(function(){
 	var bio = document.forms["bioForm"]["uploadBio"];
 	var response = "";
 	var bioSub = $.ajax({
-		url: "309/T11/userBio/" + bio,
+		url: "../../309/T11/updateProfile/" + getUsername() + bio,
 		type: "GET",
 		headers: {
 			"Authorization" : getCredentials(),
